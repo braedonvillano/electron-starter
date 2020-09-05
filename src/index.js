1656a283085b9bed889051805a78f9705576bb33
Braedon Villano <braedonvillano@braedons-mbp.myfiosgateway.com>
2020-09-05 18:23:17 -0400

    initial

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function App () {
   return (
     <div style={{width: 400, margin: '0 auto', textAlign: 'center'}}>
       <img src={web} style={{height: 100, width: 100, margin: '0 auto'}} alt=""/>
-      <div style={style}>Moar Electron!</div>
+      <div style={style}>twenty, ya know?</div>
     </div>
   )
 }
