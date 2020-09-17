98bbd8d9ce5e92556bd8bd302444ba95696c2c7b
Samantha Pelletier <samantha@samanthas-mbp.myfiosgateway.com>
2020-09-17 08:37:13 -0400

    added editor to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import web from './browser.svg'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Editor, EditorState } from 'draft-js';
+// import 'draft-js/dist/Draft.css';
+import web from './browser.svg';
 
 const style = {
   fontFamily:"-apple-system, 'Helvetica Neue', sans-serif",
@@ -10,12 +12,33 @@ const style = {
 }
 
 function App () {
+  const [editorState, setEditorState] = React.useState(
+    () => EditorState.createEmpty(),
+  );
+
+  const editor = React.useRef(null);
+
+  function focusEditor() {
+    editor.current.focus();
+  }
+
+  React.useEffect(() => {
+    focusEditor()
+  }, []);
+
   return (
     <div style={{width: 400, margin: '0 auto', textAlign: 'center'}}>
       <img src={web} style={{height: 100, width: 100, margin: '0 auto'}} alt=""/>
       <div style={style}>twenty, ya know?</div>
+      <div onClick={focusEditor}>
+        <Editor
+          ref={editor}
+          editorState={editorState}
+          onChange={editorState => setEditorState(editorState)}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 ReactDOM.render(<App />,  document.getElementById('root'));
